9561980ef41e9b366b0ef24066cd0322bb19f2e1
Victor Porton <porton.victor@gmail.com>
2022-02-12 16:00:44 +0200

    new API

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,35 @@
-/// Call Notify::notify_one() when object is dropped.
+/// Deliver () when the object is dropped.
 /// It's useful to use together with https://crates.io/crates/tokio-interruptible-future
 
-use tokio::sync::Notify;
-
 pub struct NotifyDrop<'a> {
-    notify: &'a Notify
+    notify: &'a tokio::sync::broadcast::Sender<()>,
 }
 
 impl<'a> NotifyDrop<'a> {
     #[allow(dead_code)]
-    pub fn new(notify: &'a Notify) -> Self {
+    pub fn new(notify: &'a tokio::sync::broadcast::Sender<()>) -> Self {
         Self { notify }
     }
 }
 
 impl<'a> Drop for NotifyDrop<'a> {
     fn drop(&mut self) {
-        self.notify.notify_waiters();
+        let _ = self.notify.send(());
     }
 }
 
 #[cfg(test)]
 mod tests {
     use futures::executor::block_on;
-    use tokio::sync::Notify;
 
     use crate::NotifyDrop;
 
     #[test]
     fn no_deadlock() {
-        let n = Notify::new();
-        { // block
-            let _guard = NotifyDrop::new(&n);
+        let (tx, mut rx) = tokio::sync::broadcast::channel(1);
+                { // block
+            let _guard = NotifyDrop::new(&tx);
         }
-        block_on(async { n.notified().await });
+        block_on(async { rx.recv().await.unwrap() });
     }
 }
